466a848ab9c68caec25863cb86d15d1a10d2588f
ShanshanHe <shanshanhebit@gmail.com>
2015-06-18 09:56:23 -0400

    iterative solution for Minimum Depth of Binary Tree, bfs + queue

---
+++ MinimumDepthOfBinaryTree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        //iteratively
+        //bfs, the first leaf it encouter is the leaf that has min depth
+        if( root == null ) return 0;
+        int depth = 1;
+        LinkedList<TreeNode> curr = new LinkedList<TreeNode>();
+        LinkedList<TreeNode> next = new LinkedList<TreeNode>();
+        curr.add(root);
+        while( !curr.isEmpty() ) {
+            TreeNode node = curr.poll();
+            if( node.left == null && node.right == null ) return depth;
+            if( node.left != null ) next.add(node.left);
+            if( node.right != null ) next.add(node.right);
+            if( curr.isEmpty() ) {
+                depth++;
+                curr = next;
+                next = new LinkedList<TreeNode>();
+            }
+        }
+        return depth;
+    }
+}
